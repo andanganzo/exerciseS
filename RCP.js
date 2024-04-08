6498c7e82c6d10c6817cbad51f25c27d18e25ed6
andanganzo <166250140+andanganzo@users.noreply.github.com>
2024-04-08 14:40:40 +0200

    Update RCP.js

--- RCP.js
+++ RCP.js
@@ -1,275 +1,105 @@
-let scores = {
-ties: 0,
-wins: 0,
-loses: 0
-};
 
-scores = JSON.parse(localStorage.getItem('resultCountSave'));
+    let scores = {
+    ties: 0,
+    wins: 0,
+    losses: 0
+    };
 
-        let results = '';
-        var randomNum = Math.random();
-        let randomNum2 = Math.random();
+
+    scores = JSON.parse(localStorage.getItem('resultCountSave'));
+    updateScoreElement();
+    let IntervalID;
+    let autoPlayButton = document.querySelector('.autoplayButton');
+
+    function autoplay() {
+   if (autoPlayButton.innerHTML === 'Auto play' ) {
+     autoPlayButton.innerHTML = 'Stop Autoplay'
+     IntervalID =  setInterval( function () {
+      const playerMove = pcMoves();
+      makeMove(playerMove);
+      },1000) 
+   }
+   else {
+    autoPlayButton.innerHTML = 'Auto play'
+    clearInterval(IntervalID);
+   }
+   }
+
 
     function picks (icon) {
-        document.querySelector('.You')
-                    .innerHTML = 'You: ';
-        document.querySelector('.picks-User')
-        .innerHTML = `${icon}`;
-}
-
-    function resetButton() {
-        document.querySelector('.resetButton-div')
-        .innerHTML  =
-        `<button onclick = "
-        resetScore();
-        ">Reset Scores</button>
-        `
-}
-
-
-
-function rock() {
-           picksInside(); 
-    if (picks) {
-        
-    if (randomNum === '✊') {
-        results = 'Tie'
-        scores.ties++;
- }
-        else if (randomNum === '✌️') {
-            results = 'You win'
-            resultColorScore();
-            scores.wins++;       
-        }
-        else if (randomNum === '✋') {
-            results = 'You lose'
-            
-            scores.loses++;
-        resultColorScore();
-        }
-    }   document.querySelector('.results-Div')
-            .innerHTML = results;
-            document.querySelector('.scores-Div')
-            .innerHTML = `Wins: ${scores.wins}, Ties: ${scores.ties}
-            Loses: ${scores.loses}`; 
-            localStorage.setItem('resultCountSave', JSON.stringify(scores));
+    document.querySelector('.You')
+    .innerHTML = 'You: ';
+    document.querySelector('.picks-User')
+    .innerHTML = `${icon}`;
     }
 
-function resultColorScore() {
-  
-    if (randomNum === '✊') {
-            // results = 'Tie';
-            // scores.wins++;
-            document.querySelector('.results-Div')
-            .innerHTML =` 
-            <p style = "color: yellow">${results}</p>`;
-            
-    } else if (randomNum === '✌️') {
-        // results = 'You win';
-        // scores.wins++;
-        document.querySelector('.results-Div')
-        .innerHTML =` 
-        <p style = "color: green">${results}</p>`;
-        
-    } else if (randomNum === '✋') {
-        // results = 'You lose';
-        // scores.wins++;
-        document.querySelector('.results-Div')
-        .innerHTML =` 
-        <p style = "color: rgb(147, 25, 25)">${results}</p>`;
-        
+
+
+    function resetScore() {
+
+    results = '';
+    scores.wins = 0;
+    scores.ties = 0;
+    scores.losses = 0;
+
+    document.querySelector('.results-Div')
+    .innerHTML = results;
+    document.querySelector('.scores-Div')
+    .innerHTML = `Wins: ${scores.wins}, Ties: ${scores.ties}
+    Loses: ${scores.losses}`; 
+    localStorage.setItem('resultCountSave', JSON.stringify(scores));
     }
- // document.querySelector('.scores-Div')
-    // .innerHTML = `Wins: ${scores.wins}, Ties: ${scores.ties}
-    // Loses: ${scores.loses}`; 
-    // localStorage.setItem('resultCountSave', JSON.stringify(scores));
-}
 
-    function paper() {
-        // picksInside();
-        if (picks) {
-            picks('✋');
-    if (randomNum === '✋') {
-        results = 'Tie'
-        scores.ties++;
-        }else if (randomNum === '✊') {
-            results = 'You win'
-            scores.wins++;
-        }else if (randomNum === '✌️') {
-            console.log(`${randomNum}`);
-            results = 'You lose'
-            scores.loses++;
-        }}document.querySelector('.results-Div')
-            .innerHTML = results; 
-            document.querySelector('.scores-Div')
-            .innerHTML = `Wins: ${scores.wins}, 
-            Ties: ${scores.ties}
-            Loses: ${scores.loses}`; 
-            localStorage.setItem('resultCountSave', 
-            JSON.stringify(scores));
-        }
-
-        
-        // function picksInside() {
-            
-        //     if (rock) {
-        //         picks('✊');
-        //     }
-        //     else if (paper) {
-        //         picks('✋');
-        //     }
-        //     else if (scissor) {
-        //         picks('✌️');
-        //     }
-        // }
+    function makeMove(playerMove) {
+        const computerMove = pcMoves();
+        let resultElement = document.querySelector('.results-Div');
+            let results = '';    
     
-
-    function scissor() {
-        // picksInside();
-        if (picks) {
-    if (randomNum === '✌️') {
-        results = 'Tie'
+        if (playerMove === computerMove) {
+        results = resultElement.innerHTML = ` <p style = "color: yellow">Tie.</p>`;
         scores.ties++;
-        } else if (randomNum === '✋') {
-            results = 'You win'
-            scores.wins++;
-       
-        }else if (randomNum === '✊') {
-            console.log(`${randomNum}`);
-            results = 'You lose'
-            scores.loses++;
-        }
-        
-    }document.querySelector('.results-Div')
-            .innerHTML = results; 
-            document.querySelector('.scores-Div')
-            .innerHTML = `Wins: ${scores.wins}, 
-            Ties: ${scores.ties}
-            Loses: ${scores.loses}`; 
-            localStorage.setItem('resultCountSave', 
-            JSON.stringify(scores));  
-        }
-             
+    
+        } else if (
+        (playerMove === '✊' && computerMove === '✌️') ||
+        (playerMove === '✋' && computerMove === '✊') ||
+        (playerMove === '✌️' && computerMove === '✋')
+        ) {
+        results = resultElement.innerHTML = `<p style = "color: rgb(6, 255, 6)">You win.</p>`;
 
-        function resetScore() {
-            results = '';
-            scores.wins = 0;
-            scores.ties = 0;
-            scores.loses = 0;
+        scores.wins++;
 
-            document.querySelector('.results-Div')
-            .innerHTML = results;
+        } else {
+        results = resultElement.innerHTML = `<p style = "color: red">You lose.</p>`;
+        scores.losses++;
+        }
+        const movesElement = document.querySelector('.picks-User');
+        movesElement.innerHTML = `
+        You
+        <img src="icons/${playerMove}-emoji.png" class="move-icon">
+        <img src="icons/${computerMove}-emoji.png" class="move-icon">
+        Computer
+        `; 
+        updateScoreElement();
+        }
+        function updateScoreElement() {
             document.querySelector('.scores-Div')
-            .innerHTML = `Wins: ${scores.wins}, Ties: ${scores.ties}
-            Loses: ${scores.loses}`; 
+            .innerHTML = `Wins : ${scores.wins} Ties : ${scores.ties}
+            Loses : ${scores.losses}`; 
             localStorage.setItem('resultCountSave', JSON.stringify(scores));
-        }
-
-        function pcMoves() {
-            randomNum = Math.random();
-            if (randomNum < 0.3199999999999999) {
-            randomNum = '✊'  
             }
-            else if (randomNum > 0.3199999999999999 && randomNum < 0.6999999999999999) {
-                randomNum = '✋'  
-            }
-            else if (randomNum > 0.6999999999999999 && randomNum <= 0.9999999999999999) {
-            randomNum = '✌️'
-            }
-            document.querySelector('.pc')
-                .innerHTML = 'Pc: ';
-                document.querySelector('.picks-Pc')
-                .innerHTML = ` ${randomNum}`
-        }
+      
 
-
-
-
- 
-
-
-
-
-
-        function randomNum2f() {
-            randomNum2 = Math.random();
-            if (randomNum2 < 0.3199999999999999) {
-            randomNum2 = '✋';
-            picks('✋');
-            if (randomNum === '✋') {
-                results = 'Tie'
-                scores.ties++;
-
-                }else if (randomNum === '✊') {
-                    results = 'You win'
-                    scores.wins++;
-
-                }else if (randomNum === '✌️') {
-                    console.log(`${randomNum}`);
-                    results = 'You lose'
-                    scores.loses++;
-
-                } document.querySelector('.scores-Div')
-                    .innerHTML = `Wins: ${scores.wins}, 
-                    Ties: ${scores.ties}
-                    Loses: ${scores.loses}`; 
-                    localStorage.setItem('resultCountSave', 
-                    JSON.stringify(scores))
-                }
-
-            
-            else if (randomNum2 > 0.3199999999999999 && randomNum2 <= 0.6999999999999999) {
-                randomNum2 = '✊';
-                picks('✊');
-                if (randomNum === '✊') {
-                    results = 'Tie'
-                    scores.ties++;
-             }
-                    else if (randomNum === '✌️') {
-                        results = 'You win'
-                        resultColorScore();
-                        scores.wins++;       
-                    }
-                    else if (randomNum === '✋') {
-                        results = 'You lose'
-                        scores.loses++;
-                    }document.querySelector('.results-Div')
-                        .innerHTML = results;
-                        document.querySelector('.scores-Div')
-                        .innerHTML = `Wins: ${scores.wins}, Ties: ${scores.ties}
-                        Loses: ${scores.loses}`; 
-                        localStorage.setItem('resultCountSave', JSON.stringify(scores));
-                }   
-                
-
-
-            
-            else if (randomNum2 > 0.6999999999999999 && randomNum2 <= 0.9999999999999999) {
-            randomNum2 = '✌️';
-            picks('✌️');
-            if (randomNum === '✌️') {
-                results = 'Tie'
-                scores.ties++;
-                } else if (randomNum === '✋') {
-                    results = 'You win'
-                    scores.wins++;
-               
-                }else if (randomNum === '✊') {
-                    console.log(`${randomNum}`);
-                    results = 'You lose'
-                    scores.loses++;
-                }
-                document.querySelector('.results-Div')
-                    .innerHTML = results; 
-                    document.querySelector('.scores-Div')
-                    .innerHTML = `Wins: ${scores.wins}, 
-                    Ties: ${scores.ties}
-                    Loses: ${scores.loses}`; 
-                    localStorage.setItem('resultCountSave', 
-                    JSON.stringify(scores)); 
-            }
-            
-
-        }
-
-        
+    function pcMoves() {
+    const  playerMove = Math.random();
+    let computerMove;
+    if (playerMove < 0.3199999999999999) {
+    computerMove = '✊'  
+    }
+    else if (playerMove > 0.3199999999999999 && playerMove < 0.6999999999999999) {
+    computerMove = '✋'  
+    }
+    else if (playerMove > 0.6999999999999999 && playerMove <= 0.9999999999999999) {
+    computerMove = '✌️'
+    }
+    return computerMove;
+    }
